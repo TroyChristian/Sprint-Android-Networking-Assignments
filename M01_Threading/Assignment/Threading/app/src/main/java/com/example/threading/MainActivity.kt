a84fb641cec6c2b72f1f562064ddd53341d33524
Troy Christian <apostasy8@gmail.com>
2019-09-02 23:49:12 -0400

    add progress bar to XML

--- M01_Threading/Assignment/Threading/app/src/main/java/com/example/threading/MainActivity.kt
+++ M01_Threading/Assignment/Threading/app/src/main/java/com/example/threading/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.threading
 //initial commit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
 }
+
