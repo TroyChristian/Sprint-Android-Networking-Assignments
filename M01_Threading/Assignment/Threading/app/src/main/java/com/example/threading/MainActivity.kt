bf6ef7c55d4c189b30385bf80ea6575b0ca013c7
Troy Christian <apostasy8@gmail.com>
2019-09-03 01:34:32 -0400

    add ProgressBar toggle method, add myAsyncActivity w/ over ride methods

--- M01_Threading/Assignment/Threading/app/src/main/java/com/example/threading/MainActivity.kt
+++ M01_Threading/Assignment/Threading/app/src/main/java/com/example/threading/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.threading
 //initial commit
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
+import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Toast
+import androidx.core.app.TaskStackBuilder
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +18,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fun isVisible(arg:Boolean){
+            if(arg){progressBar.visibility = VISIBLE
+            }
+            else{progressBar.visibility = INVISIBLE}
+
+
+        }
+
+        }
+
+    fun primes(): Sequence<Long> {
+        var i = 0L
+        return sequence {
+            generateSequence { i++ }
+                .filter { n -> n > 1 && ((2 until n).none { i -> n % i == 0L }) }
+                .forEach { yield(it) }
+        }
 
 
     }
 
+    val primeNumbers = primes().take(16000).joinToString(", ")
+
+    class MyAsyncTask : AsyncTask<Unit, Int, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+
+        }
+
+        override fun doInBackground(vararg p0: Unit?): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
 }
+
+
+
+
+
+
+
 
