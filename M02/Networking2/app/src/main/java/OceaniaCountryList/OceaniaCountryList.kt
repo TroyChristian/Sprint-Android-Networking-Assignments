6ca1e3a6ba65f3642e5e6c05a614c43b8563211b
Troy Christian <apostasy8@gmail.com>
2019-09-04 09:24:16 -0400

    Code in place, review with V

--- M02/Networking2/app/src/main/java/OceaniaCountryList/OceaniaCountryList.kt
+++ M02/Networking2/app/src/main/java/OceaniaCountryList/OceaniaCountryList.kt
@@ -1,4 +1,11 @@
 package OceaniaCountryList
+
+import com.example.networking2.CountriesAPI
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 //constructor arg is just a placeholder due to Kotlin requirements for data class
 
 data class OceaniaCountryList(val country:  List<OceaniaCountry>)
@@ -9,3 +16,22 @@ data class OceaniaCountry(   val countryName: String,
                              val alpha2code: String,
                              val capital: String,
                              val subregion: String)
+
+class OceaniaCountriesRetriever {
+
+    fun getOceaniaCountries(): Call<OceaniaCountryList> {
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://restcountries.eu/rest/v2/all/") //was BASE_URL
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        val countriesAPI = retrofit.create(CountriesAPI::class.java)
+
+        return countriesAPI.getCountries()
+    }
+
+}
