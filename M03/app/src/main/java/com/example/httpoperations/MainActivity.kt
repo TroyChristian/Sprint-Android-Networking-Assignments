8bec58e308a76ea67343693b27182b4ef4d77606
Troy Christian <apostasy8@gmail.com>
2019-09-05 01:07:22 -0400

    XML added, OnClickListeners added,

--- M03/app/src/main/java/com/example/httpoperations/MainActivity.kt
+++ M03/app/src/main/java/com/example/httpoperations/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.httpoperations
 //initial commit
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_get.setOnClickListener{
+            intent = Intent(this, GetActivity::class.java)
+            startActivity(intent)
+        }
+        btn_put.setOnClickListener{
+            intent = Intent(this, PutActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_post.setOnClickListener{
+            intent = Intent(this, PostActivity::class.java)
+            startActivity(intent)
+
+        }
+        btn_delete.setOnClickListener{
+            intent = Intent(this, DeleteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
