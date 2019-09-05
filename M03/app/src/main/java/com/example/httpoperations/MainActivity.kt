81ee90a67e2e355af051ef08d11c6a2d1e6f5d82
Troy Christian <apostasy8@gmail.com>
2019-09-05 01:59:30 -0400

    add isConnected() refactor onClickListeners, add dependencies, add permissions.

--- M03/app/src/main/java/com/example/httpoperations/MainActivity.kt
+++ M03/app/src/main/java/com/example/httpoperations/MainActivity.kt
@@ -1,32 +1,60 @@
 package com.example.httpoperations
-//initial commit
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+    fun isNetworkConnected(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo?.isConnected == true}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn_get.setOnClickListener{
-            intent = Intent(this, GetActivity::class.java)
-            startActivity(intent)
+            if (!isNetworkConnected()) {
+                Snackbar.make(it, "Please check your internet connection and try again", Snackbar.LENGTH_LONG).show()
+            } else {
+
+            }
+
+            startActivity(Intent(this@MainActivity, GetActivity::class.java))
         }
         btn_put.setOnClickListener{
-            intent = Intent(this, PutActivity::class.java)
-            startActivity(intent)
+            if (!isNetworkConnected()) {
+                Snackbar.make(it, "Please check your internet connection and try again", Snackbar.LENGTH_LONG).show()
+            } else {
+
+            }
+
+            startActivity(Intent(this@MainActivity, PutActivity::class.java))
         }
 
         btn_post.setOnClickListener{
-            intent = Intent(this, PostActivity::class.java)
-            startActivity(intent)
+            if (!isNetworkConnected()) {
+                Snackbar.make(it, "Please check your internet connection and try again", Snackbar.LENGTH_LONG).show()
+            } else {
 
+            }
+
+            startActivity(Intent(this@MainActivity, PostActivity::class.java))
         }
         btn_delete.setOnClickListener{
-            intent = Intent(this, DeleteActivity::class.java)
-            startActivity(intent)
-        }
+            if (!isNetworkConnected()) {
+                Snackbar.make(it, "Please check your internet connection and try again", Snackbar.LENGTH_LONG).show()
+            } else {
+
+            }
+
+            startActivity(Intent(this@MainActivity, GetActivity::class.java))
     }
-}
+
+
+}}
