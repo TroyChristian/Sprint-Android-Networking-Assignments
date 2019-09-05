8dec0b3980876968d3fe8cc614262fbbcd2f1558
Troy Christian <apostasy8@gmail.com>
2019-09-05 02:19:00 -0400

    Add JsonPlaceHolderApi, add Factory class

--- M03/app/src/main/java/Retrofit/JsonPlaceHolderApi.kt
+++ M03/app/src/main/java/Retrofit/JsonPlaceHolderApi.kt
@@ -1,9 +1,15 @@
 package Retrofit
 
 import Model.Employee
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.Retrofit
+
+import java.util.concurrent.TimeUnit
 
 interface JsonPlaceHolderApi {
 
@@ -18,3 +24,33 @@ interface JsonPlaceHolderApi {
 
     @GET("employee")
     fun getEmployeeByAge(employeeId: String)}
+
+class Factory {
+
+    companion object {
+
+        private const val BASE_URL = "https://demo8143297.mockable.io"
+
+        fun create(): JsonPlaceHolderApi {
+
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BASIC
+            logger.level = HttpLoggingInterceptor.Level.BODY
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .retryOnConnectionFailure(false)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            return retrofit.create(JsonPlaceHolderApi::class.java)
+        }
+    }
+}
